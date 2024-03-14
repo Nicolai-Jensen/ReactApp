af37231d7d0abbd0a705825dd9a36f290455c75b
Nicolai-Jensen <nicolaijensen11@gmail.com>
2024-03-14 13:02:38 +0100

    login
    username and password input fields
    
    they have no functions yet

---
+++ src/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+    </div>
+  );
+}
