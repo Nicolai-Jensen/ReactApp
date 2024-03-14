c297510937e72702bfa874b2f3ec3845720241e4
Nicolai-Jensen <nicolaijensen11@gmail.com>
2024-03-14 13:15:27 +0100

    updated login to have a submit button
    submit eligible

--- src/Login.js
+++ src/Login.js
@@ -12,23 +12,37 @@ export function Login() {
     setPassword(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // You can handle form submission here, such as sending the data to a backend server
+    console.log("Username:", username);
+    console.log("Password:", password);
+    // Reset the form fields after submission
+    setUsername("");
+    setPassword("");
+  };
+
   return (
-    <div>
-      <label htmlFor="username">Username:</label>
-      <input
-        type="text"
-        id="username"
-        value={username}
-        onChange={handleUsernameChange}
-      />
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        value={password}
-        onChange={handlePasswordChange}
-      />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
