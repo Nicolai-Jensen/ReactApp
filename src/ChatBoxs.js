c3d8f3cae85e7e2bad9c845665ef0e2da799e8df
theAshamedsleeper <thorbjorn114@gmail.com>
2024-03-15 12:28:43 +0100

    just trying things

--- src/ChatBoxs.js
+++ src/ChatBoxs.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 export function DrawingChatBox(props){
+    
+    
     return(
         <>
             <h1>CHAT BOX STUFF</h1>
--- src/gameWorld.js
+++ src/gameWorld.js
@@ -21,15 +21,6 @@ class DrawGameWorld extends React.Component{
             gameServerObj: null
         };
     }
-    componentDidMount() {
-        // Using the useGameServer hook here
-        if (this.state.loginSuc && this.state.token) {
-            const gameServer = useGameServer("http://react.tsanas.com/gamehub", this.state.token, this.handleConnectionClosed);
-
-            // Store the gameServer object in state
-            this.setState({ gameServerObj: gameServer }); 
-        }
-    }
 
     handleLoginSuccess = (token) => {
         this.setState({
@@ -42,14 +33,29 @@ class DrawGameWorld extends React.Component{
         // Handle connection closed
     };
     
+    componentDidMount() {
+        // Using the useGameServer hook here
+        if (this.state.loginSuc && this.state.token) {
+            const gameServer = useGameServer("http://react.tsanas.com/gamehub", this.state.token, this.handleConnectionClosed);
 
+            // Store the gameServer object in state
+            this.setState({ gameServerObj: gameServer }); 
+        }
+        console.log(this.state.gameServerObj);
+    }
+    
+    handleChat(props){
+        this.state.gameServerObj.onEvent()=> {
+            console.log("Something happend")
+        }
+    }
 
     render(){
         return(
             <>
                 <h1>Title of the is {this.state.game}</h1>
                 {!this.state.loginSuc && <Login onLoginSuccess={this.handleLoginSuccess} />}
-                {this.state.loginSuc && <DrawingChatBox/>}
+                {this.state.loginSuc && <DrawingChatBox props/>}
             </>
         );
     }
