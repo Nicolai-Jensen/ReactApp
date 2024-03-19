aeaf035077b0d0eda95a3a94c68188711ede8d55
Nicolai-Jensen <nicolaijensen11@gmail.com>
2024-03-19 13:35:52 +0100

    A very long and arduous journey
    Finally should have World update working
    
    Grid component needs work to fix non-ground components

--- src/game.js
+++ src/game.js
@@ -10,7 +10,7 @@ export function GameState(props) {
 
     return (
         <>
-        <World />
+        <World gameServer = {gameServer}/>
         <DrawingChatBox gameServer = {gameServer}/>
         </>
     )
