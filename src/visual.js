3af27c7d775185cdde2ab90aaa1829b2e824a621
theAshamedsleeper <thorbjorn114@gmail.com>
2024-03-20 09:30:55 +0100

    a little fix to visuals
    so it draws the right stuff just not the right place

--- src/visual.js
+++ src/visual.js
@@ -3,85 +3,82 @@ import './game.css';
 
 const GridContainer = (props) => {
     return (
-      <div className="grid-container">
-        {/* Render ground tiles */}
-        {props.ground.map((tile, index) => (
-          <img
-            key={index}
-            className="grid-item ground"
-            src={`./tiles/tile_${tile}.png`}
-          />
-        ))}
-  
-        {/* Render other tiles */}
-        {props.movables.map((tile, index) => (
-        <img
-            key={index}
-            alt=""
-            className={`grid-item ${tile.type} ${tile.flipped ? 'flip' : ''}`}
-            style={{ left: !isNaN(tile.x) ? tile.x * 48 : 0, top: !isNaN(tile.y) ? tile.y * 48 : 0 }}
-            src={`./tiles/tile_${tile.src}.png`}
-        />
-        ))};
+        <div className="grid-container">
+            {/* Render ground tiles */}
+            {props.ground.map((tile, index) => (
+                <img
+                    key={index}
+                    className="grid-item ground"
+                    src={`./tiles/tile_${tile}.png`}
+                />
+            ))}
 
-        {props.clutter.map((tile, index) => (
-        <img
-            key={index}
-            alt=""
-            className={`grid-item ${tile.type} ${tile.flipped ? 'flip' : ''}`}
-            style={{ left: !isNaN(tile.x) ? tile.x * 48 : 0, top: !isNaN(tile.y) ? tile.y * 48 : 0 }}
-            src={`./tiles/tile_${tile.src}.png`}
-        />
-        ))};
+            {/* Render other tiles */}
+            {props.movables.map((tile, index) => (
+                <img
+                    key={index}
+                    className={`grid-item ${tile.id} ${tile.flipped ? 'flip' : ''}`}
+                    style={{ left: !isNaN(tile.xpos) ? tile.xpos * 48 : 0, top: !isNaN(tile.ypos) ? tile.ypos * 48 : 0 }}
+                    src={`./tiles/tile_${tile.tile}.png`}
+                />
+            ))};
 
-        {
-        props.effects.map((tile, index) => (
-            <img
-                key={index}
-                alt=""
-                className={`grid-item ${tile.type} ${tile.flipped ? 'flip' : ''}`}
-                style={{ left: !isNaN(tile.x) ? tile.x * 48 : 0, top: !isNaN(tile.y) ? tile.y * 48 : 0 }}
-                src={`./tiles/tile_${tile.src}.png`}
-            />
-        ))}
-      </div>
+            {props.clutter.map((tile, index) => (
+                <img
+                    key={index}
+                    className={`grid-item ${tile.id} ${tile.flipped ? 'flip' : ''}`}
+                    style={{ left: !isNaN(tile.xpos) ? tile.xpos * 48 : 0, top: !isNaN(tile.ypos) ? tile.ypos * 48 : 0 }}
+                    src={`./tiles/tile_${tile.tile}.png`}
+                />
+            ))};
+
+            {
+                props.effects.map((tile, index) => (
+                    <img
+                        key={index}
+                        className={`grid-item ${tile.id} ${tile.flipped ? 'flip' : ''}`}
+                        style={{ left: !isNaN(tile.xpos) ? tile.xpos * 48 : 0, top: !isNaN(tile.ypos) ? tile.ypos * 48 : 0 }}
+                        src={`./tiles/tile_${tile.tile}.png`}
+                    />
+                ))}
+        </div>
     );
 };
-  
+
 export function World(props) {
-const gameServerObj = props.gameServer;
-const [info, setInfo] = useState({});
-const [ground, setGround] = useState([]);
-const [movables, setMovables] = useState([]);
-const [clutter, setClutter] = useState([]);
-const [effects, setEffects] = useState([]);
+    const gameServerObj = props.gameServer;
+    const [info, setInfo] = useState({});
+    const [ground, setGround] = useState([]);
+    const [movables, setMovables] = useState([]);
+    const [clutter, setClutter] = useState([]);
+    const [effects, setEffects] = useState([]);
 
-useEffect(() => {
-    gameServerObj.onEvent("WorldUpdate", response => {
-        if(response === undefined)
-        {
-            return;
-        }
-        if (response.info)
-            setInfo(response.info)
-        if (response.movables)
-            setMovables(response.movables)
-        if (response.ground)
-            setGround(response.ground)
-        if (response.clutter)
-            setClutter(response.clutter)
-        if (response.effects)
-            setEffects(response.effects)
+    useEffect(() => {
+        gameServerObj.onEvent("WorldUpdate", response => {
+            if (response === undefined) {
+                return;
+            }
+            if (response.info)
+                setInfo(response.info)
+            if (response.movables)
+                setMovables(response.movables)
+            if (response.ground)
+                setGround(response.ground)
+            if (response.clutter)
+                setClutter(response.clutter)
+            if (response.effects)
+                setEffects(response.effects)
 
-    });
-}, );
+        });
+    },);
+
+    //console.log(movables);
 
 
-
-return (
-    <>
-    <GridContainer info={info} ground={ground} movables={movables} clutter={clutter} effects={effects} /> {/* Include the grid container component */}
-    </>
-);
+    return (
+        <>
+            <GridContainer info={info} ground={ground} movables={movables} clutter={clutter} effects={effects} /> {/* Include the grid container component */}
+        </>
+    );
 }
 
